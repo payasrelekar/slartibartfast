3c39cc7319ffdb93095bd2c1962deb0fe6d984e5
payas <relekarpayas@gmail.com>
2017-08-14 15:34:27 +0530

    Complete upto Problem Statement-2 in Lab Excercise-2 Added Constructor and overload it. Added accessor and mutator methods and initialized objects through them. Complete problem statement 2.

--- src/myDate/myDate.java
+++ src/myDate/myDate.java
@@ -4,25 +4,61 @@ public class myDate {
 	
 	public int day, month, year;
 	
-	public void initDate(int d, int m, int y) {
+	//Constructor : default
+	public myDate() {
 		
-		this.day = d;
-		this.month = m;
-		this.year = y;
+		setDay(1);
+		setMonth(1);
+		setYear(1970);
 	}
 	
+	//Constructor : with parameters
+	public myDate(int d, int m, int y) {
+		
+		setDay(d);
+		setMonth(m);
+		setYear(y);
+	}
+	
+	//accessor and mutator methods for date values	
+	public int getDay() {
+		return day;
+	}
+
+	public void setDay(int day) {
+		this.day = day;
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	//toString function for wasy output
 	public String toString() {
 		
 		return "Date is "+this.day+"/"+this.month+"/"+this.year;
 	}
 	
+	//MAIN
 	public static void main(String[] args) {
 		
 		myDate date1 = new myDate();
-		
-		date1.initDate(12, 13, 14);
+		myDate date2 = new myDate(12, 13, 14);
 		
 		System.out.println(date1);
+		System.out.println(date2);
 		
 	}
 
